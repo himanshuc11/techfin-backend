135063e82709bc5db52fdb59d4861ba43cb84569
himanshuc11 <himanshuchhatpar@gmail.com>
2025-06-14 15:53:26 +0530

    feat: add WebSocket support for database client configuration

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,18 @@
 import { DATABASE_URL } from "#constants/env.js";
 import { drizzle } from "drizzle-orm/neon-serverless";
 import { Pool } from "@neondatabase/serverless";
+import { Client, neonConfig } from "@neondatabase/serverless";
+import ws from "ws";
 
-const sql = new Pool({ connectionString: DATABASE_URL! });
+neonConfig.webSocketConstructor = ws;
+
+const client = new Client(process.env.DATABASE_URL);
+client.neonConfig.webSocketConstructor = ws;
+
+const sql = new Pool({
+  connectionString: DATABASE_URL!,
+});
+
 const db = drizzle(sql);
 
 export { db };
